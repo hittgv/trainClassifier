0120d64402ab11730a9abf11ec99d64d9c411058
Bhasin, Krishan <Krishan.Bhasin@capitalone.com>
2017-03-19 11:20:00 +0000

    first version of model generation code & the model itself

--- trainClassifer.py
+++ trainClassifer.py
@@ -1,17 +1,45 @@
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
-# load the iris datasets
-dataset = datasets.load_iris()
-# fit a logistic regression model to the data
+import json
+import numpy as np
+import pandas as pd
+from sklearn.externals import joblib
+
+# read it as a rowwise dataset and lable it as fake
+data_fake = pd.read_json('fake.json').transpose()
+data_fake['fake_flag'] = True
+
+data_real = pd.read_json('real.json').transpose()
+data_real['fake_flag'] = False
+
+all_data = data_fake.append(data_real)
+
+print all_data
+
+X = all_data.drop('fake_flag', axis=1).drop('url', axis=1)
+y = all_data['fake_flag']
+
 model = LogisticRegression()
 
 
-model.fit(dataset.data, dataset.target)
-print(model)
-# make predictions
-expected = dataset.target
-predicted = model.predict(dataset.data)
-# summarize the fit of the model
-print(metrics.classification_report(expected, predicted))
-print(metrics.confusion_matrix(expected, predicted))
+model.fit(X, y)
+
+print(model.get_params())
+
+expected = pd.DataFrame(y)
+
+predicted = pd.DataFrame(model.predict(X))
+
+probs = model.predict_proba(X)
+
+print(probs)
+
+joblib.dump(model, 'fakeNews.pkl')
+
+# print(pd.concat([expected, predicted]))
+
+# print(metrics.classification_report(expected, predicted))
+# print(metrics.confusion_matrix(expected, predicted))
+
+# print(model.score(X, y))
