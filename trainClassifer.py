ee4e4f270a1e02695127a1290c728150d6d0dba4
Bhasin, Krishan <Krishan.Bhasin@capitalone.com>
2017-03-19 13:00:50 +0000

    added equation coefficients

--- trainClassifer.py
+++ trainClassifer.py
@@ -5,6 +5,8 @@ import json
 import numpy as np
 import pandas as pd
 from sklearn.externals import joblib
+import matplotlib.pyplot as plt
+from time import sleep
 
 # read it as a rowwise dataset and lable it as fake
 data_fake = pd.read_json('fake.json').transpose()
@@ -22,24 +24,42 @@ y = all_data['fake_flag']
 
 model = LogisticRegression()
 
-
 model.fit(X, y)
 
-print(model.get_params())
+# print(model.get_params())
 
 expected = pd.DataFrame(y)
 
 predicted = pd.DataFrame(model.predict(X))
 
 probs = model.predict_proba(X)
+probs_log = model.predict_log_proba(X)
 
-print(probs)
 
-joblib.dump(model, 'fakeNews.pkl')
+print model.get_params()
 
-# print(pd.concat([expected, predicted]))
 
-# print(metrics.classification_report(expected, predicted))
-# print(metrics.confusion_matrix(expected, predicted))
+print list(X)
+print model.coef_
 
-# print(model.score(X, y))
+# plt.scatter(probs_log, probs, alpha=0.5)
+# plt.show()
+
+
+
+# print(probs)
+
+# joblib.dump(model, 'fakeNews_v2.pkl')
+
+# # print(pd.concat([expected, predicted]))
+
+# # print(metrics.classification_report(expected, predicted))
+# # print(metrics.confusion_matrix(expected, predicted))
+
+# # print(model.score(X, y))
+
+# # plt.scatter(X['dailyTimeOnSite'], y, alpha=0.5)
+# # plt.show()
+
+# plt.scatter(X['internalLinks'], probs, alpha=0.5)
+# plt.show()
